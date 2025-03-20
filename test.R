0b8ec381908c9a48a0fa5459155a28f1e299f922
Nils Nerhus Rørstad <nnrorstad@gmail.com>
2025-03-20 15:51:59 +0100

    Updated the references, the renv.lock and the research design chapters.

---
+++ test.R
@@ -0,0 +1,11 @@
+# Load your package
+library(napr)
+
+# If you're just checking the data structure
+str(nap_data)
+
+# Or to view the first few rows
+head(nap_data)
+
+# If you want to create a local copy for manipulation
+my_data <- nap_data
