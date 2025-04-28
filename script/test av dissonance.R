7edc1b7c1552a6a867918c012b794bfe64e456e9
Nils Nerhus Rørstad <nnrorstad@gmail.com>
2025-04-28 15:01:49 +0200

    Updated some of the documentation

--- script/test av dissonance.R
+++ script/test av dissonance.R
@@ -1,5 +1,12 @@
-topic_results <- readRDS("data/topic_proportions.rds")
+
+# Source the function
+if(!exists("find_entropy")) source("script/find_entropy.R")
+
+topic_props <- readRDS("data/topic_props.rds")
+
 topic_data <- topic_results$data
+
+topic_data
 
 # For calculating entropy by country
 country_dissonance <- find_entropy(topic_data, 
@@ -25,7 +32,7 @@ formatted_table <- topic_data %>%
   select(country_name, Topic, Proportion) %>%
   mutate(Proportion = sprintf("%.4f", Proportion))
 
-view(formatted_table)
+formatted_table
 
 # Overall topic entropy
 topic_entropy <- find_entropy(topic_data, value_col = "Proportion")
@@ -38,3 +45,11 @@ regional_entropy
 # Country-level entropy
 country_entropy <- find_entropy(topic_data, value_col = "Proportion", group_by_col = "country_name")
 country_entropy
+
+load(topic_props.rds)
+
+
+country_entropy <- find_entropy(topic_data, value_col = "Proportion", group_by_col = "country_name")
+
+country_entropy
+
