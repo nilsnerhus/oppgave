872b591315f0c639adab1e973b383459557c2607
Nils Nerhus Rørstad <nnrorstad@gmail.com>
2025-04-29 10:16:14 +0200

    Refactoring

--- scripts/everything_together.R
+++ scripts/everything_together.R
@@ -6,7 +6,8 @@
 # Create output directory
 dir.create("data", recursive = TRUE, showWarnings = FALSE)
 
-# Source function files if not already loaded
+# Source function files if not already loaded¨
+if(!exists("utils")) source("scripts/utils.R")
 if(!exists("scrape_web")) source("scripts/scrape_web.R")
 if(!exists("extract_pdfs")) source("scripts/extract_pdfs.R")
 if(!exists("add_metadata")) source("scripts/add_metadata.R")
@@ -14,23 +15,14 @@ if(!exists("prepare_corpus")) source("scripts/prepare_corpus.R")
 if(!exists("find_best_k")) source("scripts/find_best_k.R")
 if(!exists("topic_model")) source("scripts/extract_topic_props.R")
 
-
-message("Starting pipeline...")
-
 # Step 1: Scrape the UNFCCC website
-message("Scraping web...")
 exclude_countries <- c("Uruguay") # Does not have a national plan, just many sectoral
 web <- scrape_web(exclude_countries = exclude_countries)
-message("Scraper completed!")
 
 # Step 2: Extract the pdfs from the web
-message("Extracting pdfs...")
 pdfs <- extract_pdfs(web)
-message("Extration completed!")
-
 
 # Step 3: Add metadata
-message("Adding metadata...")
 ## Define lists of SIDS and LLDC countries, since no public repositories keep the information
 lldc <- c("AFG", "ARM", "AZE", "BFA", "BDI", "CAF", "TCD", "ETH", "KAZ", 
           "KGZ", "LAO", "LSO", "MWI", "MLI", "MDA", "MNG", "NPL", "NER", 
@@ -51,23 +43,13 @@ ldc <- c("AFG", "AGO", "BGD", "BEN", "BFA", "BDP", "BOL", "BIH", "BWA", "CAF",
          "TGO", "TON", "TJK", "UGA", "UZB", "VUT", "YEM", "ZMB", "ZWE")
 
 metadata <- add_metadata(pdfs, sids_list = sids, lldc_list = lldc, ldc_list = ldc)
-message("Addition completed!")
-message("Results saved to the 'data' directory")
 
 # Step 4: Prepare corpus
-message("Preparing corpus...")
 nap_stops <- c("mr", "https", "la", "yet", "de", "i.e", "yr", "tion", "des", "8.5", "svg")
-corpus <- prepare_corpus(nap_data, custom_stopwords = nap_stops)
-message("Corpus prepared!")
+corpus <- prepare_corpus(metadata, custom_stopwords = nap_stops)
 
 # Step 5: Find optimal topic count
-message("Finding the best k...")
 best_k <- find_best_k(corpus)
-message("Found best k! It is", best_k$best_k, " ")
 
 # Step 6: Extract topic proportions
-message("Extracting topic proportions...")
 topic_props <- extract_topic_props(best_k)
-message("Topic proportions extracted!")
-
-message(" NAP processing pipeline completed successfully and results saved to the 'data' directory ")
